a9d7d124578d6916f2f79475761080fc6f0f452e
karthikyits <karthik@yitsol.com>
2017-06-01 20:19:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var proxy = require('proxy-agent');
+var bucketName = 'yits';
 
 var app = module.exports = loopback();
 var multer  =   require('multer');
@@ -13,9 +15,11 @@ var storage =   multer.diskStorage({
     callback(null, file.originalname );
   }
 });
+
 var upload = multer({ storage : storage }).array('userPhoto',5);
 app.start = function() {
   // start the web server
+  //uploadtos3();
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
@@ -32,11 +36,45 @@ app.get('/',function(req,res){
 app.post('/api/photo',function(req,res){
   upload(req,res,function(err) {
     //console.log(req.body);
-    //console.log(req.files);
+    console.log(req.files);
     if(err) {
       return res.end("Error uploading file.");
+    } else {
+      console.log("uploading... to s3");
+      var AWS = require('aws-sdk');
+      var fs = require('fs');
+      AWS.config.loadFromPath(__dirname+ "/s3-config.json");
+      /*AWS.config.update({
+        httpsOptions: { agent: proxy('<proxy_url>')},
+        httpOptions: { agent: proxy('<<proxy_url>>') }
+      });*/
+      var s3Bucket = new AWS.S3( { params: {Bucket: bucketName} } );
+
+      fs.readFile(req.files[0].path, function (err, data) {
+        var params  = {
+          Key: req.files[0].originalname,
+          Body: data,
+          ContentType: req.files[0].mimetype,
+          ACL:'public-read'
+
+        };
+        s3Bucket.putObject(params , function(err, data){
+          if (err) {
+            console.log(err);
+            console.log('Error uploading data: ', params);
+          } else {
+            console.log('Successfully uploaded the image!');
+            var link = s3Bucket.endpoint.href+bucketName +'/'+ params.Key;
+            console.log('link -> ' + link);
+            res.write("File is Uploaded, Download link : ");
+            res.end(link);
+          }
+        });
+      });
+
+
     }
-    res.end("File is uploaded");
+
   });
 });
 // Bootstrap the application, configure models, datasources and middleware.
